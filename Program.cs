ac5ba484b893ffee47d3664fcf3bed97c040d6f8
faka1995 <60807513+faka1995@users.noreply.github.com>
2020-02-19 17:01:37 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BuzzFizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("unesi broj");
+            int brojkorisnika = int.Parse(Console.ReadLine());
+
+            for (int broj = 0; broj <= brojkorisnika; broj++)
+            {
+                if ((broj % 3 == 0) && (broj % 5 == 0))
+                {
+                    Console.WriteLine(broj + "fizzBuzz");
+                }
+
+                else if (broj % 3 == 0)
+                {
+                    Console.WriteLine(broj + "Fizz");
+                }
+                else if (broj % 5 == 0)
+                {
+                    Console.WriteLine(broj + "buzz");
+                }
+                else
+                {
+                    Console.WriteLine(broj);
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
